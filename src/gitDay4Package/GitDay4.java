a8c2f97acc16f9c87bd0693785b78ff276cf5cb9
suheyla <suheyla.kalkn@gmail.com>
2020-03-16 18:29:43 -0500

    first commit from local master

---
+++ src/gitDay4Package/GitDay4.java
@@ -0,0 +1,27 @@
+package gitDay4Package;
+
+public class GitDay4 {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("local first commit master branch");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
